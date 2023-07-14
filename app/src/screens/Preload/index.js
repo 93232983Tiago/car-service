420191ff7193cd314bf0e2f0aaf8364db6a01255
TIAGO FREITAS <tiagofreitas@gascom.com.br>
2023-07-14 13:39:51 -0300

    AsyncStorage function token :iphone:

--- app/src/screens/Preload/index.js
+++ app/src/screens/Preload/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, Image } from "react-native";
 import { Container, LoadingIcon, Teslapng } from './styles';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 import Carlogo from '../../../assets/carrepair.svg'
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    // função usando o asyncstorage para validar o token de acesso
+    useEffect(()=>{
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token')
+            if(token !== null){
+                // função timeout para a visualização do preload
+                setTimeout(()=>{  
+                    
+                }, 1000);
+            } else {
+                setTimeout(() =>{
+                    navigation.navigate('SignIn'); //se não houver o token irá para a tela de Login
+                }, 1000);
+            }
+        }
+        checkToken();
+    }, []);
+
     return(
         <Container>
-            <Carlogo width="100%" height="150" fill="#FFF"/>
+            <Carlogo width="50%" height="100" fill="#FFF"/>
             <LoadingIcon size="large" color="#FFF"/>
         </Container>   
     );
